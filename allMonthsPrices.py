2bc107112c4e07292f1331fe5fa7376976531197
kelvingakuo <kelvingakuo@gmail.com>
2018-04-02 20:32:50 +0300

    Prices for all months

---
+++ allMonthsPrices.py
@@ -0,0 +1,56 @@
+import time
+import requests
+import sys
+import pathlib
+from selenium import webdriver
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+import csv
+
+# 1. DOWNLOAD THE EXCEL FILE
+
+url = "https://www.eia.gov/dnav/ng/hist/rngwhhdM.htm"
+
+# 1.1 Configure Selenium appropriately
+path = 'path/to/driver' #Path to where the Selenium web driver is
+
+download_dir = "path/to/download/directory" # Download to this directory
+chrome_options = webdriver.ChromeOptions()
+preferences = {"download.default_directory": download_dir ,
+               "directory_upgrade": True,
+               "safebrowsing.enabled": True }
+chrome_options.add_experimental_option("prefs", preferences)
+driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=path)
+
+driver.get(url);
+
+# 1.2 Download file
+time.sleep(5)  # Wait 5 seconds
+alll = driver.find_elements_by_class_name('crumb')
+alll[1].click()
+
+time.sleep(20) #Wait for download
+# 2. EXTRACT SPECIFIC DATA NEEDED
+
+# 2.1 Read from Excel file
+df = pd.read_excel('RNGWHHDm.xls', sheetname = 'Data 1')
+
+dates = df['Back to Contents'].values  # Read dates column
+dates = dates[2:]  # Actual content start from index 2
+prices = df['Data 1: Henry Hub Natural Gas Spot Price (Dollars per Million Btu)'].values  # Read prices column
+prices= prices[2:] # Actual content start from index 2
+
+rows = zip(dates, prices)
+
+# 2.2 Write to CSV file
+with open("allMonthlyPrices.csv", "a") as dump: #Open the csv file to write the data to
+		writer = csv.writer(dump)
+		writer.writerow(['Year', 'Price'])
+
+		for row in rows:
+			writer.writerow(row)
+
+
+print("CSV SUCCESSFULLY POPULATED")
+
